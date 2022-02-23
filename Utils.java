f6d327c4d58fd553825bafb776ca89408a65b7cd
KKK-^ <69491052+Kei-7777@users.noreply.github.com>
2022-02-23 16:14:53 +0900

    Create Utils.java

---
+++ Utils.java
@@ -0,0 +1,35 @@
+package net.kunmc.lab.%package%;
+
+import org.bukkit.Bukkit;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Utils {
+
+    public static List<String> nl = Arrays.asList("");
+
+    public static void a(String... m) {
+        for (String s : m) {
+            Bukkit.getServer().broadcastMessage(s); // 广播消息
+        }
+    }
+
+    public static List<String> s(List<String> l) {
+        Collections.sort(l);
+        return l;
+    }
+
+    public static List<String> cl(List<String> l, String a) {
+        if(a.length() < 1) return s(l);
+        List<String> r = new java.util.ArrayList<>();
+        for (String s : l) {
+            if (s.startsWith(a)) {
+                r.add(s);
+            }
+        }
+        return s(r);
+    }
+
+}
